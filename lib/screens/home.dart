5402ba1aa9051765a445e13f07884686ed5d615e
jardianoalmeida <61389908+jardianoalmeida@users.noreply.github.com>
2020-05-04 10:51:45 -0300

    Criando o Layout da Tela Principal

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,6 +4,24 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25.0,
+          child: Image.asset("images/yt_logo_rgb_dark.png"),
+        ),
+        backgroundColor: Colors.black87,
+        elevation: 0.0,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(icon: Icon(Icons.search), onPressed: (){}),
+          IconButton(icon: Icon(Icons.star), onPressed: (){}),
+        ],
+      ),
+      body: Container(),
+    );
   }
 }
